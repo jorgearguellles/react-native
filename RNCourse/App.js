8680baffaff245424bc4a9538a0f405b73882496
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-01-02 21:23:59 -0500

    🥕 Dive deep on Flexbox

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput placeholder='Your course goal!' style={styles.textInput}/>
-        <Button title='Add goal'/>
+        <Button title='Add goal' style={styles.button}/>
       </View>
       <View>
         <Text>List of goals...</Text>
@@ -17,11 +17,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer:{
-    padding: 50
+    paddingTop: 100,
+    padding: 50,
+  },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center'
   },
   textInput:{
     borderWidth: 1,
-    borderColor: 'blue',
+    borderColor: '#cccccc',
+    width: '80%',
     padding: 15,
   }
 });
